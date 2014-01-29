15c1dda4c16d71a7c7f456027dcd30624a40d86c
mrbakkep <thomasbakkepedersen@gmail.com>
2014-01-29 10:48:40 +0100

    Delete ex2.py

--- ex2.py
+++
@@ -1,9 +0,0 @@
-# A comment, this is so you can read your program later.
-# Anything after the # is ignored by python.
-
-print "I could have code like tihs." # annd the comment after is ifnored
-
-# You can also use a comment to "disable" or comment out a piece of cde:
-# print "Thos von't run."
-
-print "This will run."
